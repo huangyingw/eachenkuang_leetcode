c83fcf5ca9ed6520b5cbf4409e358baa1cbaa9e3
Ying Huang <huangyingw@gmail.com>
2019-06-09 16:24:07 -0700

    n

--- code/989#Add to Array-Form of Integer.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def addToArrayForm(self, A: 'List[int]', K: 'int') -> 'List[int]':
-        b = ''.join(list(map(str,A)))
-        c = list(str(int(b) + K))
-        return list(map(int,c))
