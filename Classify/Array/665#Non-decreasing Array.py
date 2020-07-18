bc5bbd4fddb28e7d2785d316b0d546f2ff4bab55
Ying Huang <huangyingw@gmail.com>
2020-07-17 20:49:54 -0700

    n

--- Classify/Array/665#Non-decreasing Array.py
+++ Classify/Array/665#Non-decreasing Array.py
@@ -1,10 +1,6 @@
-# https://leetcode.com/problems/non-decreasing-array/description/
 class Solution:
     def checkPossibility(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+        
         p = None
         for i in range(len(nums) - 1):
             if nums[i] > nums[i+1]:
@@ -14,5 +10,3 @@ class Solution:
         return (p is None or p == 0 or p == len(nums)-2 or 
                 nums[p-1] <= nums[p+1] or nums[p] <= nums[p+2])
     
-
-
